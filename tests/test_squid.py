28ffc170ffa19f55e9eb12946198da258864d1f5
Krzysztof Dzienis <kdzienis@gmail.com>
2022-07-14 14:01:07 +0200

    events per second test

--- tests/test_squid.py
+++ tests/test_squid.py
@@ -10,4 +10,6 @@ def test_init_file():
     assert type(factory.dataframe) == pd.core.frame.DataFrame
     assert factory.most_frequent_IP().json() == '{"action": "MostFrequentIP", "files": ["tests/test_logs/access1.log"], "ip": "127.0.0.1"}'
     assert factory.least_frequent_IP().json() == '{"action": "LeastFrequentIP", "files": ["tests/test_logs/access1.log"], "ip": "219.103.55.23"}'
+    assert factory.total_amount_of_bytes_exchanged().json() == '{"action": "TotalAmountOfBytesExchanged", "files": ["tests/test_logs/access1.log"], "no_bytes": 1415286389}'
+    assert factory.events_per_second().json() == '{"action": "EventsPerSecond", "files": ["tests/test_logs/access1.log"], "no_events": 3.560953678887375}'
 
